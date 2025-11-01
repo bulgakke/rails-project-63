b346680ac67fe010e0c4546334e69579e72f9c09
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-01 21:05:47 +0400

    feat: empty form

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,13 @@ require_relative "hexlet_code/version"
 # Main module
 module HexletCode
   autoload(:Tag, "hexlet_code/tag")
+
+  def self.form_for(object, attributes = {})
+    url = attributes.delete(:url) || "#"
+    default_method = "post"
+    attributes[:action] ||= url
+    attributes[:method] ||= default_method
+
+    HexletCode::Tag.build("form", attributes) {}
+  end
 end
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe HexletCode do
   it "has a version number" do
     expect(HexletCode::VERSION).not_to be nil
   end
+
+  describe ".form_for" do
+    subject { HexletCode.form_for(object, attributes) }
+
+    let(:object) { "something" }
+
+    context "without attributes" do
+      it "returns a form tag with default attributes" do
+        expect(HexletCode.form_for("something")).to eq("<form action=\"#\" method=\"post\"></form>")
+      end
+    end
+
+    context "with attributes" do
+      let(:attributes) { { action: "http://example.com", method: "put" } }
+
+      it "returns a form tag with specified attributes" do
+        expect(subject).to eq("<form action=\"http://example.com\" method=\"put\"></form>")
+      end
+    end
+  end
 end
