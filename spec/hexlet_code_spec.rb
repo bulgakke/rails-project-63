2cc65916ee1379ec0a45c939356b9a02ce5b71e1
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-01 23:26:18 +0400

    fix: rubocop config

--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe HexletCode do
-  it "has a version number" do
+  it 'has a version number' do
     expect(HexletCode::VERSION).not_to be nil
   end
 
-  describe ".form_for" do
-    let(:object) { "something" }
-    let(:attributes) { { a: "b" } }
+  describe '.form_for' do
+    let(:object) { 'something' }
+    let(:attributes) { { a: 'b' } }
     let(:block) { proc {} }
 
-    it "calls HexletCode::Form.new" do
+    it 'calls HexletCode::Form.new' do
       expect(HexletCode).to receive(:form_for).with(object, attributes, &block)
       HexletCode.form_for(object, attributes, &block)
     end
